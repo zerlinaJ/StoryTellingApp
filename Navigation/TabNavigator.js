5f1a1e4b43665ffac40e62af2827b6c271b39ce2
Zerlina Jacob <zerlinaj@gmail.com>
2021-11-28 19:53:39 +0530

    styling changed

--- Navigation/TabNavigator.js
+++ Navigation/TabNavigator.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {StyleSheet} from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feed from '../Screens/Feed';
 import CreateStory from '../Screens/CreateStory';
+import { RFValue } from "react-native-responsive-fontsize";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,6 +12,8 @@ export default class BottomTabNavigator extends React.Component {
   render(){
     return (
         <Tab.Navigator
+        labeled={false}
+        barStyle={styles.bottomTabStyle}
           screenOptions={({ route }) => ({
             tabBarIcon: ({ focused, color, size }) => {
               let iconName;
@@ -20,13 +23,11 @@ export default class BottomTabNavigator extends React.Component {
               else if (route.name === 'CreateStory') {
                 iconName = focused ? 'create' : 'create-outline';
               }
-              return <Ionicons name={iconName} size={size} color={color} />
+              return <Ionicons name={iconName} size={RFValue(25)} color={color} style={styles.icons}/>
             }
           })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-          }}
+          activeColor={"#ee8249"}
+          inactiveColor={"gray"}
         >
           <Tab.Screen name="StoryFeed" component={Feed} />
           <Tab.Screen name="CreateStory" component={CreateStory} />
@@ -35,3 +36,18 @@ export default class BottomTabNavigator extends React.Component {
 
   }
 }
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "8%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
+  }
+});
