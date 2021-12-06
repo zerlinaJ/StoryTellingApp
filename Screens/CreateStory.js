5fc2b3768030e214fc67b1ed28a63f06ac4064ed
Zerlina Jacob <zerlinaj@gmail.com>
2021-12-06 17:22:23 +0530

    Tried Picker instead of dropdown picker

--- Screens/CreateStory.js
+++ Screens/CreateStory.js
@@ -1,84 +1,119 @@
 import React, { Component } from "react";
-import { Text, View, Image, StyleSheet, SafeAreaView, Platform, StatusBar, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  TextInput,
+  Dimensions,
+  Picker
+} from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import DropDownPicker from "react-native-dropdown-picker";
+
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 
 let customFonts = {
-	"Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
 };
 
 export default class CreateStory extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			fontsLoaded: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      previewImage: "image_1",
+      dropdownHeight: 40
+    };
+  }
 
-	async _loadFontsAsync() {
-		await Font.loadAsync(customFonts);
-		this.setState({ fontsLoaded: true });
-	}
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
 
-	componentDidMount() {
-		this._loadFontsAsync();
-	}
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
 
-	render() {
-		if (!this.state.fontsLoaded) {
-			return <AppLoading />
-		} else {
-			return (
-			<View style={styles.container}>
-				<SafeAreaView style={styles.droidSafeArea} />
-				<View style={styles.appTitle}>
-					<View style={styles.appIcon}>
-						<Image
-							source={require("../assets/logo.png")}
-							style={styles.iconImage}
-						></Image>
-					</View>
-					<View style={styles.appTitleTextContainer}>
-						<Text style={styles.appTitleText}>New Story</Text>
-					</View>
-				</View>
-			</View>
-			)
-		}
-	}
+  render() {
+return(
+	<Picker
+	style={styles.picker}
+	selectedValue={this.state.selectedValue}
+	onValueChange={(value) => {this.setState({selectedValue: value})}}
+	itemStyle={styles.pickerItem}
+>
+	<Picker.Item label="Apple" value="Apple" />
+	<Picker.Item label="Orange" value="Orange" />
+	<Picker.Item label="Banana" value="Banana" />
+	<Picker.Item label="Kiwi" value="Kiwi" />
+</Picker>
+)
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#15193c"
-	},
-	droidSafeArea: {
-		marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
-	},
-	appTitle: {
-		flex: 0.07,
-		flexDirection: "row"
-	},
-	appIcon: {
-		flex: 0.3,
-		justifyContent: "center",
-		alignItems: "center"
-	},
-	iconImage: {
-		width: "100%",
-		height: "100%",
-		resizeMode: "contain"
-	},
-	appTitleTextContainer: {
-		flex: 0.7,
-		justifyContent: "center"
-	},
-	appTitleText: {
-		color: "white",
-		fontSize: RFValue(28),
-		fontFamily: "Bubblegum-Sans"
-	}
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  fieldsContainer: {
+    flex: 0.85
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain"
+  },
+  inputFont: {
+    height: RFValue(40),
+    borderColor: "white",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "white",
+    fontFamily: "Bubblegum-Sans"
+  },
+  inputFontExtra: {
+    marginTop: RFValue(15)
+  },
+  inputTextBig: {
+    textAlignVertical: "top",
+    padding: RFValue(5)
+  }
+});
