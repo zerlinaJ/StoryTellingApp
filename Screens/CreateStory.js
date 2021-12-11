803ac2f74e839e2293549010c461c1650e987cc2
Zerlina Jacob <zerlinaj@gmail.com>
2021-12-11 11:34:35 +0530

    Working DropDownPicker code

--- Screens/CreateStory.js
+++ Screens/CreateStory.js
@@ -73,101 +73,103 @@ export default class CreateStory extends Component {
 								source={preview_images[this.state.previewImage]}
 								style={styles.previewImage}
 							></Image>
-							<View style={{ height: RFValue(this.state.dropdownHeight), alignItems:'center' }}>
-								<Picker
+							<View style={{ height: RFValue(this.state.dropdownHeight), alignItems: 'center' }}>
+								{/* <Picker
 									style={{ backgroundColor: 'white' }}
 									selectedValue={this.state.previewImage}
 									onValueChange={(item) => { this.setState({ previewImage: item }) }}
-									itemStyle={{flex:1}}
-								// items={[
-								// 	{ label: "Image 1", value: "image_1" },
-								// 	{ label: "Image 2", value: "image_2" },
-								// 	{ label: "Image 3", value: "image_3" },
-								// 	{ label: "Image 4", value: "image_4" },
-								// 	{ label: "Image 5", value: "image_5" }
-								//   ]}
-								//   defaultValue={this.state.previewImage}
-								//   containerStyle={{
-								//     height: 40,
-								//     borderRadius: 20,
-								//     marginBottom: 10
-								//   }}
-								//   onOpen={() => {
-								//     this.setState({ dropdownHeight: 170 });
-								//   }}
-								//   onClose={() => {
-								//     this.setState({ dropdownHeight: 40 });
-								//   }}
-								//   style={{ backgroundColor: "transparent" }}
-								//   itemStyle={{
-								//     justifyContent: "flex-start"
-								//   }}
-								//   dropDownStyle={{ backgroundColor: "#2f345d" }}
-								//   labelStyle={{
-								//     color: "white",
-								//     fontFamily: "Bubblegum-Sans"
-								//   }}
-								//   arrowStyle={{
-								//     color: "white",
-								//     fontFamily: "Bubblegum-Sans"
-								//   }}
-								//   onChangeItem={item =>
-								//     this.setState({
-								//       previewImage: item.value
-								//     })
-								//   }
+									itemStyle={{flex:1}} */}
+								<DropDownPicker
+									items={[
+										{ label: "Image 1", value: "image_1" },
+										{ label: "Image 2", value: "image_2" },
+										{ label: "Image 3", value: "image_3" },
+										{ label: "Image 4", value: "image_4" },
+										{ label: "Image 5", value: "image_5" }
+									]}
+									defaultValue={this.state.previewImage}
+									containerStyle={{
+										height: 40,
+										borderRadius: 20,
+										marginBottom: 10,
+										width: '20%'
+									}}
+									onOpen={() => {
+										this.setState({ dropdownHeight: 100 });
+									}}
+									onClose={() => {
+										this.setState({ dropdownHeight: 40 });
+									}}
+									style={{ backgroundColor: "transparent" }}
+									itemStyle={{
+										justifyContent: "center",
+									}}
+									dropDownStyle={{ backgroundColor: "#2f345d" }}
+									labelStyle={{
+										color: "white",
+										fontFamily: "Bubblegum-Sans"
+									}}
+									arrowStyle={{
+										color: "white",
+										fontFamily: "Bubblegum-Sans"
+									}}
+									onChangeItem={item =>
+										this.setState({
+											previewImage: item.value
+										})
+									}
 								>
-									<Picker.Item label="Image_1" value="image_1" />
+									{/* <Picker.Item label="Image_1" value="image_1" />
 									<Picker.Item label="Image_2" value="image_2" />
 									<Picker.Item label="Image_3" value="image_3" />
-									<Picker.Item label="Image_4" value="image_4" />
-								</Picker>
+									<Picker.Item label="Image_4" value="image_4" /> */}
+								</DropDownPicker>
 							</View>
 
-						 <TextInput
-                style={styles.inputFont}
-                onChangeText={title => this.setState({ title })}
-                placeholder={"Title"}
-                placeholderTextColor="white"
-              />
+							<TextInput
+								style={styles.inputFont}
+								onChangeText={title => this.setState({ title })}
+								placeholder={"Title"}
+								placeholderTextColor="white"
+							/>
 
-              <TextInput
-                style={[
-                  styles.inputFont,
-                  styles.inputFontExtra,
-                  styles.inputTextBig
-                ]}
-                onChangeText={description => this.setState({ description })}
-                placeholder={"Description"}
-                multiline={true}
-                numberOfLines={4}
-                placeholderTextColor="white"
-              />
-              <TextInput
-                style={[
-                  styles.inputFont,
-                  styles.inputFontExtra,
-                  styles.inputTextBig
-                ]}
-                onChangeText={story => this.setState({ story })}
-                placeholder={"Story"}
-                multiline={true}
-                numberOfLines={20}
-                placeholderTextColor="white"
-              />
+							<TextInput
+								style={[
+									styles.inputFont,
+									styles.inputFontExtra,
+									styles.inputTextBig
+								]}
+								onChangeText={description => this.setState({ description })}
+								placeholder={"Description"}
+								multiline={true}
+								numberOfLines={4}
+								placeholderTextColor="white"
+							/>
+							<TextInput
+								style={[
+									styles.inputFont,
+									styles.inputFontExtra,
+									styles.inputTextBig
+								]}
+								onChangeText={story => this.setState({ story })}
+								placeholder={"Story"}
+								multiline={true}
+								numberOfLines={20}
+								placeholderTextColor="white"
+							/>
 
-              <TextInput
-                style={[
-                  styles.inputFont,
-                  styles.inputFontExtra,
-                  styles.inputTextBig
-                ]}
-                onChangeText={moral => this.setState({ moral })}
-                placeholder={"Moral of the story"}
-                multiline={true}
-                numberOfLines={4}
-                placeholderTextColor="white"
-              /> 
+							<TextInput
+								style={[
+									styles.inputFont,
+									styles.inputFontExtra,
+									styles.inputTextBig
+								]}
+								onChangeText={moral => this.setState({ moral })}
+								placeholder={"Moral of the story"}
+								multiline={true}
+								numberOfLines={4}
+								placeholderTextColor="white"
+							/>
 						</ScrollView>
 					</View>
 					<View style={{ flex: 0.08 }} />
