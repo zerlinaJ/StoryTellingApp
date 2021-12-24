bed04da028a0c4ff67e03ca3ff36ebbe7dea6958
Zerlina Jacob <zerlinaj@gmail.com>
2021-12-24 12:32:13 +0530

    c-84 final - expo-speech installed and speech working now

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCoG0db9Ln-ljxF_aOfjcP0BEDPSbU0K7A",
+    authDomain: "storytelling-app-e2121.firebaseapp.com",
+    projectId: "storytelling-app-e2121",
+    storageBucket: "storytelling-app-e2121.appspot.com",
+    messagingSenderId: "658382714539",
+    appId: "1:658382714539:web:bc1fb81dc176dd3988e9ee"
+  };
