ec2d3d001839fcaec4f429727b0c84fbed5ca667
Zerlina Jacob <zerlinaj@gmail.com>
2022-01-03 12:39:48 +0530

    C-87 Light/Dark theme implemented in Profile Screen

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -73,8 +73,13 @@ export default class Profile extends Component {
 			return <AppLoading />
 		} else {
 			return (
-				<View style={styles.container}>
-					<SafeAreaView style={styles.droidSafeArea} />
+				// <View style={styles.container}>
+				<View
+					style={
+						this.state.light_theme ? styles.containerLight : styles.container
+					}
+				>
+					< SafeAreaView style={styles.droidSafeArea} />
 					<View style={styles.appTitle}>
 						<View style={styles.appIcon}>
 							<Image
@@ -83,7 +88,16 @@ export default class Profile extends Component {
 							></Image>
 						</View>
 						<View style={styles.appTitleTextContainer}>
-							<Text style={styles.appTitleText}>Storytelling App</Text>
+							{/* <Text style={styles.appTitleText}>Storytelling App</Text> */}
+							<Text
+								style={
+									this.state.light_theme
+										? styles.appTitleTextLight
+										: styles.appTitleText
+								}
+							>
+								Storytelling App
+							</Text>
 						</View>
 					</View>
 					<View style={styles.screenContainer}>
@@ -92,15 +106,33 @@ export default class Profile extends Component {
 								source={{ uri: this.state.profile_image }}
 								style={styles.profileImage}
 							></Image>
-							<Text style={styles.nameText}>{this.state.name}</Text>
+							{/* <Text style={styles.nameText}>{this.state.name}</Text> */}
+							<Text
+								style={
+									this.state.light_theme
+										? styles.nameTextLight
+										: styles.nameText
+								}
+							>
+								{this.state.name}
+							</Text>
 						</View>
 						<View style={styles.themeContainer}>
-							<Text style={styles.themeText}>Dark Theme</Text>
+							{/* <Text style={styles.themeText}>Dark Theme</Text> */}
+							<Text
+								style={
+									this.state.light_theme
+										? styles.themeTextLight
+										: styles.themeText
+								}
+							>
+								{this.state.light_theme ? 'Light Theme' : 'Dark Theme'}
+							</Text>
 							<Switch
 								style={{
 									transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }]
 								}}
-								trackColor={{ false: "#767577", true: "white" }}
+								trackColor={{ false: "#767577", true: this.state.light_theme ? "#eee" : "white" }}
 								thumbColor={this.state.isEnabled ? "#ee8249" : "#f4f3f4"}
 								ios_backgroundColor="#3e3e3e"
 								onValueChange={() => this.toggleSwitch()}
@@ -110,7 +142,7 @@ export default class Profile extends Component {
 						<View style={{ flex: 0.3 }} />
 					</View>
 					<View style={{ flex: 0.08 }} />
-				</View>
+				</View >
 			);
 		}
 	}
@@ -120,6 +152,10 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#15193c"
+	},
+	containerLight: {
+		flex: 1,
+		backgroundColor: "white"
 	},
 	droidSafeArea: {
 		marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
@@ -147,6 +183,11 @@ const styles = StyleSheet.create({
 		fontSize: RFValue(28),
 		fontFamily: "Bubblegum-Sans"
 	},
+	appTitleTextLight: {
+		color: "black",
+		fontSize: RFValue(28),
+		fontFamily: "Bubblegum-Sans"
+	},
 	screenContainer: {
 		flex: 0.85
 	},
@@ -166,6 +207,12 @@ const styles = StyleSheet.create({
 		fontFamily: "Bubblegum-Sans",
 		marginTop: RFValue(10)
 	},
+	nameTextLight: {
+		color: "black",
+		fontSize: RFValue(40),
+		fontFamily: "Bubblegum-Sans",
+		marginTop: RFValue(10)
+	},
 	themeContainer: {
 		flex: 0.2,
 		flexDirection: "row",
@@ -174,6 +221,12 @@ const styles = StyleSheet.create({
 	},
 	themeText: {
 		color: "white",
+		fontSize: RFValue(30),
+		fontFamily: "Bubblegum-Sans",
+		marginRight: RFValue(15)
+	},
+	themeTextLight: {
+		color: "black",
 		fontSize: RFValue(30),
 		fontFamily: "Bubblegum-Sans",
 		marginRight: RFValue(15)
